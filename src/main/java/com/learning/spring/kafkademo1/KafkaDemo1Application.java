e9cd16e9bd03e892b8dd839bd07afc5541aec4b5
i321172 <jason.guan01@sap.com>
2019-02-12 13:53:56 +0800

    first

---
+++ src/main/java/com/learning/spring/kafkademo1/KafkaDemo1Application.java
@@ -0,0 +1,14 @@
+package com.learning.spring.kafkademo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDemo1Application.class, args);
+	}
+
+}
+
